3600347134e1a5e497acbe55cef66f4f2e44e041
root <12345678>
2020-05-09 03:03:18 +0200

    Kata Java par Hasler

---
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -0,0 +1,72 @@
+package com.gildedrose;
+
+class GildedRose {
+    Item[] items;
+
+    public GildedRose(Item[] items) {
+        this.items = items;
+    }
+
+    public void updateQuality() {
+        for (Item item : items) {
+            if (!item.name.equals("Aged Brie")
+                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality > 0) {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.quality = item.quality - 1;
+                    }
+                }
+            } else increaseQuality(item);
+
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.sellIn = item.sellIn - 1;
+            }
+
+            if (item.sellIn < 0) {
+                handlePastSellin(item);
+            }
+        }
+    }
+
+    private void handlePastSellin(Item item) {
+        if (!item.name.equals("Aged Brie")) {
+            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality > 0) {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.quality = item.quality - 1;
+                    }
+                }
+            } else {
+                item.quality = 0;
+            }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+        }
+    }
+
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+
+            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                handleBackstagePass(item);
+            }
+        }
+    }
+
+    private void handleBackstagePass(Item backstagePass) {
+        if (backstagePass.sellIn < 11) {
+            if (backstagePass.quality < 50) {
+                backstagePass.quality = backstagePass.quality + 1;
+            }
+        }
+
+        if (backstagePass.sellIn < 6) {
+            if (backstagePass.quality < 50) {
+                backstagePass.quality = backstagePass.quality + 1;
+            }
+        }
+    }
+}
