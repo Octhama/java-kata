e125d398d683e9b6d0e3f79e4faaf2489e745d97
root <12345678>
2020-05-15 04:00:19 +0200

    last update of Hasler Java-Kata

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -30,10 +30,8 @@ class GildedRose {
     private void PastSellin(Item item) {
         if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 
-                if (item.sellIn > 0) {
-                    item.sellIn = item.sellIn - 1;
-                } else {
-                    item.sellIn = item.sellIn + 1;
+                if (item.quality > 0) {
+                    item.quality = item.quality + 1;
                 }
         } else {
             item.sellIn = 0;
